9cbcadeb178924576de10a7a65121c17a1f2cde6
locoda <locoda@foxmail.com>
2017-11-09 22:38:06 -0500

    .py version of plot function

---
+++ RMSE_plot.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib import rcParams
+
+sns.set_context("talk")
+sns.set_style("whitegrid")
+rcParams['patch.force_edgecolor'] = True
+# get_ipython().magic('matplotlib inline')
+
+
+df = pd.read_csv('rmse-step1.csv')
+df = df.set_index('Unnamed: 0')
+df = df.transpose()
+df.columns.name = 'features (m)'
+# df.head()
+
+
+plt.suptitle('RMSE vs. Features ',fontsize=14)
+plt.title('(Test Step of 1, m from 1 to 24)', fontsize=10)
+sns.tsplot(data=df.as_matrix(), time=df.columns, value='RMSE')
+plt.show()
+
